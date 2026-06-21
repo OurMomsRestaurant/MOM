2e1365915e2aed6d3291a9653aa5f0e476b7ccee
Our Moms Restaurant <info@ourmomsrestaurant.com>
2026-06-21 09:28:03 -0500

    Restore Burger Blast with music and sound effects

--- sw.js
+++ sw.js
@@ -1,67 +1,19 @@
-const CACHE_NAME = "our-moms-crush-v5";
-
-const CORE_ASSETS = [
-  "./",
-  "./index.html",
-  "./our-moms-match-rush-mobile.html",
-  "./manifest.webmanifest",
-  "./assets/our-moms-badge-logo.png",
-  "./assets/our-moms-lady-logo.png",
-  "./assets/icons/apple-touch-icon.png",
-  "./assets/icons/icon-192.png",
-  "./assets/icons/icon-512.png",
-  "./assets/music/chowder.mp3",
-  "./assets/music/play-me-like-that-video-game.mp3",
-  "./assets/music/powerhouse-1984.mp3",
-  "./assets/sfx/game-over-voice.mp3"
-];
+const CACHE_NAME = "burger-blast-cleanup-v1";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(CORE_ASSETS))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      ))
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.registration.unregister())
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
-  const request = event.request;
-  if (request.method !== "GET") return;
-
-  const url = new URL(request.url);
-  if (url.origin !== self.location.origin) return;
-
-  if (request.mode === "navigate") {
-    event.respondWith(
-      fetch(request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", copy));
-          return response;
-        })
-        .catch(() => caches.match("./index.html"))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(request)
-      .then(cached => cached || fetch(request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
-        return response;
-      }))
-  );
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request));
 });
